f7cda9ea00a19a6160ebff43fd5b6161c3e85baf
anderson-alves427 <oliveiraalves427@gmail.com>
2023-03-05 10:06:25 -0300

    style: add colors

--- src/shared/theme/colors.ts
+++ src/shared/theme/colors.ts
@@ -1,4 +1,10 @@
 export const colors = {
   primary: '#36D6AD',
+  secondary: '#3772FF',
   text: '#FFFFFF',
+  coral: '#3772FF',
+  creme: '#FCF0E3',
+  cinza: '#737380',
+  cinzaClaro: '#BCBCBC',
+  cinzaCLaroFundos: '#F7F7F7',
 };
